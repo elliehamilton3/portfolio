d480fb97a24bbc48b27b5d5d888a02faf6b93160
Ellie Hamilton <elliehamilton@Ellies-MacBook-Pro.local>
2018-10-24 12:44:10 +0100

    Update circle config

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,12 @@
-#!/usr/bin/env sh
-
-# abort on errors
 set -e
 
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git config --global user.email "$GH_EMAIL"
+git config --global user.name "$GH_NAME" 
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:elliehamilton3/elliehamilton3.github.io.git master
 
 cd -
