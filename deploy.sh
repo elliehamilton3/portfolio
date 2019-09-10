2d65d0e928c7a8bb6b14aef31640de977c61ffca
Ellie <ellie@Ellies-MacBook-Pro.local>
2019-09-10 19:14:45 +1000

    Updating persistance

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,8 @@ git config --global user.email "$GH_EMAIL"
 git config --global user.name "$GH_NAME" 
 
 git init
-git add -A
+git add docs/.vuepress/dist
 git commit -m 'deploy'
-
-git push --force --quiet git@github.com:elliehamilton3/elliehamilton3.github.io.git master
+git subtree push --prefix docs/.vuepress/dist origin gh-pages
 
 cd -
