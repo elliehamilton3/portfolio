3345006a0b41cb37174acf8eae0607fed6fc3f30
Ellie <ellie@Ellies-MacBook-Pro.local>
2019-09-10 19:29:06 +1000

    Updating persistance

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ git config --global user.name "$GH_NAME"
 git init
 git add -f docs/.vuepress/dist
 git commit -m 'deploy'
-git subtree push --prefix docs/.vuepress/dist origin gh-pages
 git filter-branch -f --prune-empty --subdirectory-filter docs/.vuepress/dist && git push -f origin gh-pages
 
 cd -
