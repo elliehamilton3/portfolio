866e97e016b4dd4718a95cbb1351282e94bc7f6e
Ellie <ellie@Ellies-MacBook-Pro.local>
2019-09-10 19:39:47 +1000

    Bug fixing pushing dist to gh pages

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ git config --global user.name "$GH_NAME"
 git init
 git add -f docs/.vuepress/dist
 git commit -m 'deploy'
-git filter-branch -f --prune-empty --subdirectory-filter docs/.vuepress/dist && git push -f origin master
+git filter-branch -f --prune-empty --subdirectory-filter docs/.vuepress/dist 
+git push --force --quiet git@github.com:elliehamilton3/elliehamilton3.github.io.git 
 
 cd -
